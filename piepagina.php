17a6198308b30a55d73e344f6f7ebee881405bc4
yerarbp <yerar_gbp@hotmail.com>
2019-06-11 09:25:17 -0500

    Create piepagina.php

---
+++ piepagina.php
@@ -0,0 +1,5 @@
+<footer class="container-fluid text-center" style="background-color:#ECF0F1  ; color:#17202A;padding: 15px;">
+  <h6> INE,  Instituto Nacional Electoral, Manlio Fabio Altamirano 114, Centro, 91000 Xalapa Enríquez, Ver. </h6>
+<h6> Teléfono 228 818 2080</h6>
+<h6> Ext 107</h6>
+</footer>
