8aff590d743ca15902b3d51f4650dc15924c607c
yerarbp <yerar_gbp@hotmail.com>
2019-06-11 09:08:33 -0500

    Create config2.php

---
+++ config2.php
@@ -0,0 +1,15 @@
+ <?php 
+$idmodulo= $_GET['parametro_id'];
+include "conexion.php";
+
+ $sql =("select * from remesas.modulo where remesas.modulo.idmodulo=$idmodulo");
+                                      
+                         $query = $con -> query($sql);
+                        $r=$query->fetch_array();
+
+                        echo $configuracionmodulo=$r["totalequipos"];
+
+
+                                         
+                        
+  ?>
