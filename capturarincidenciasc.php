e247cb2a64dcd737327ca00f78dfd19322d6c012
yerarbp <yerar_gbp@hotmail.com>
2019-09-11 15:00:22 -0500

    Update capturarincidenciasc.php

--- capturarincidenciasc.php
+++ capturarincidenciasc.php
@@ -186,7 +186,7 @@ if($_POST['guardar']){
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon"   type ="image/PNG" href="img/INE2.PNG">
+        <link rel="icon"   type ="image/PNG" href="img/INE2.png">
 
         <title>Registro Incidencias </title>
 
