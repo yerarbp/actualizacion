b9dbff5fc752fe3c3e3cb18d1f9da5939e28187f
yerarbp <yerar_gbp@hotmail.com>
2019-09-11 15:02:09 -0500

    Update usuarios2.php

--- usuarios2.php
+++ usuarios2.php
@@ -45,7 +45,7 @@ if($_POST['guardar']){
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon"   type ="image/PNG" href="img/INE2.PNG">
+        <link rel="icon"   type ="image/PNG" href="img/INE2.png">
 
         <title>Asignación de Distritos y Módulos </title>
 
